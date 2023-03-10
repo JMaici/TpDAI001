ba82e502743fca5351d18263126d22844f48cf32
JOAQUIN MACEIRA <47129158@ort>
2023-03-10 11:53:43 -0300

    Completed Controller (needs testing)

--- Controllers/PizzasController.cs
+++ Controllers/PizzasController.cs
@@ -10,30 +10,64 @@ namespace TPDAI001.Controllers{
         [HttpGet]
         public IActionResult GetAll()
         {
-
+            List<Pizza> listaP = BD.ListarPizzas();
+            return Ok(listaP);
            
         }
         [HttpGet("{id}")]
         public IActionResult GetById(int id)
         {
+             if (id < 1)
+        {
+            return BadRequest();
+        }
 
-           
+        Pizza p = BD.ObtenerPizzaPorID(id);
+
+        if (p == null)
+        {
+            return NotFound();
+        }
+
+        return Ok(p);
+       
         }
         [HttpPost]
         public IActionResult Create(Pizza pizza)
         {
-
+        if (pizza.Nombre == null || pizza.Nombre == "")
+        {
+            return BadRequest();
+        }
+      
+        BD.AgregarPizza(pizza);
+        return Ok();
            
         }
         [HttpPut("{id}")]
         public IActionResult Update(int id, Pizza pizza)
         {
-
-           
+            if(id != pizza.Id){
+                return BadRequest();
+            }
+            if(BD.ObtenerPizzaPorID(id) == null){
+                return NotFound();
+            }
+            BD.ModificarPizza(id, pizza);
+            return Ok();
         }
         [HttpDelete("{id}")]
         public IActionResult DeleteById(int id)
         {
+            if(id <= 0){
+                return BadRequest();
+            }
+            if(BD.ObtenerPizzaPorID(id) == null){
+                return NotFound();
+            }
+            BD.EliminarPizza(id);
+            return Ok();
+            
 
            
         }
