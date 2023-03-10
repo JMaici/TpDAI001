d0c8d3f75926563c0806f79132628a03954af0cc
JOAQUIN MACEIRA <47129158@ort>
2023-03-10 11:27:30 -0300

    Completed BD.cs (not tested)

--- Models/BD.cs
+++ Models/BD.cs
@@ -5,5 +5,50 @@ using System.Collections.Generic;
 
 public class BD
 {
-    
+    private static string _connectionString = @"Server=A-PHZ2-CIDI-004;DataBase=TPDAI001;Trusted_Connection=True";
+    public static List<Pizza> ListarPizzas(){
+        List<Pizza> listaP =  new List<Pizza>(); 
+        string sql = "SELECT * FROM Pizzas";
+         using (SqlConnection db = new SqlConnection(_connectionString))
+        {
+            listaP = db.Query<Pizza>(sql).ToList();
+        }
+        return listaP;
+    }
+    public static Pizza ObtenerPizzaPorID(int Id)
+    {
+        Pizza P = null;
+        string sql = "SELECT * FROM Pizza WHERE Pizza.Id = @pId";
+        using (SqlConnection db = new SqlConnection(_connectionString))
+        {
+            P = db.QueryFirstOrDefault<Pizza>(sql, new { pId = Id });
+        }
+        return P;
+    }
+    public static void EliminarPizza(int Id)
+    {
+        string sql = "DELETE FROM Pizza WHERE Id = @pId";
+        using (SqlConnection db = new SqlConnection(_connectionString))
+        {
+            db.Execute(sql, new { pId = Id });
+        }
+    }
+    public static void AgregarPizza(Pizza P)
+    {
+        string sql = "INSERT INTO Pizza VALUES (@pNombre, @pLibreGluten, @pImporte, @pDescripcion)";
+        using (SqlConnection db = new SqlConnection(_connectionString))
+        {
+            db.Execute(sql, new { pNombre = P.Nombre, pLibreGluten = P.LibreGluten, pImporte = P.Importe, pDescripcion = P.Descripcion });
+        }
+    }
+    public static void ModificarPizza(int Id, Pizza P)
+    {
+        string sql = "UPDATE Pizza Set Nombre = @pNombre, LibreGluten = @pLibreGluten, Importe = @pImporte, Descripcion = @pDescripcion WHERE Id = @pId";
+        using (SqlConnection db = new SqlConnection(_connectionString))
+        {
+            db.Execute(sql, new { pId = Id, pNombre = P.Nombre, pLibreGluten = P.LibreGluten, pImporte = P.Importe, pDescripcion = P.Descripcion });
+        }
+    }
+
+
 }
